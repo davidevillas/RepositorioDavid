0d00b53271ae4a18a60a2f99855b7a6aed652d93
David <davidevillas@gmail.com>
2024-07-10 14:29:17 -0500

    Nuevo commit

---
+++ LogGRC.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Inicializa el navegador y abre la página web
+
+options = webdriver.EdgeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Edge(options=options)
+#driver = webdriver.Edge()
+driver.get('https://servicios-de-valoracion.apps.bancolombia.com/Evaluaciones')
+
+# Localiza el botón por su ID y realiza un clic
+boton = driver.find_element_by_class_name('botonPositivo btn btn-primary')##driver.find_element(By.ID, 'id_del_boton')
+boton.click()
+
+# Cierra el navegado
+driver.quit()
